1ad375ba644007471b565ee4097f9f040e0e843b
Peter <peter@ourstudio.se>
2023-11-28 15:48:54 +0100

    adds custom filters, removes build

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,75 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
-import "@wayke-se/components-react/dist/assets/default.css";
-import WaykeComposite, {
-  WaykeCompositeWithProviderProps,
-} from "@wayke-se/components-react";
+import '@wayke-se/components-react/dist/assets/default.css';
+import WaykeComposite, { WaykeCompositeWithProviderProps } from '@wayke-se/components-react';
 
 const settings: WaykeCompositeWithProviderProps = {
   composite: {
-    marketCode: "NO",
+    marketCode: 'NO',
+    filterList: [
+      {
+        filterName: 'manufacturer',
+        displayName: 'Produsent',
+      },
+      {
+        filterName: 'modelSeries',
+        displayName: 'Modell',
+      },
+      {
+        filterName: 'fuelType',
+        displayName: 'Drivmiddel',
+      },
+      {
+        filterName: 'gearboxType',
+        displayName: 'Girkasse',
+      },
+      {
+        filterName: 'branch',
+        displayName: 'Forhandler',
+      },
+      {
+        filterName: 'color',
+        displayName: 'Farge',
+      },
+      {
+        filterName: 'environmentClass',
+        displayName: 'Miljøklasse',
+      },
+      {
+        filterName: 'properties.segment',
+        displayName: 'Segment',
+      },
+      {
+        filterName: 'drivingWheel',
+        displayName: 'Drivhjul',
+      },
+      {
+        filterName: 'price',
+        displayName: 'Pris',
+      },
+      {
+        filterName: 'businessLeasingPrice',
+        displayName: 'Business leasing',
+      },
+      {
+        filterName: 'odometerValueAsKm',
+        displayName: 'Måleravlesning',
+      },
+      {
+        filterName: 'modelYear',
+        displayName: 'Modellår',
+      },
+    ],
   },
   provider: {
-    graphQlUrl: "https://gql.wayke.se/query",
-    url: "https://api.wayke.se/vehicles",
-    urlMlt: "https://api.wayke.se/vehicles-mlt-ext",
+    graphQlUrl: 'https://gql.wayke.se/query',
+    url: 'https://api.wayke.se/vehicles',
+    urlMlt: 'https://api.wayke.se/vehicles-mlt-ext',
   },
 };
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<WaykeComposite {...settings} />);
